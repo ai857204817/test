70b3580a4b7e61858c5875495cf62b39cbcb6e6c
zengshaohui <zengshaohui@lockin.com>
2025-05-23 14:23:27 +0800

    【修改】添加代码

--- main.c
+++ main.c
@@ -7,5 +7,8 @@ int main(int args, char **argv)
 	printf("hello world 222\r\n");
 	printf("one\r\n");
 	printf("two\r\n");
+	printf("three\r\n");
+	printf("four\r\n");
+	printf("five\r\n");
 	return 0;
 }
